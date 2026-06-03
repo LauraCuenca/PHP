19c2e22d6605a87468910a06d11bd92c9deb78de
Magali Scotto <maguiscotto@gmail.com>
2026-06-02 22:08:54 -0300

    Cambio leve backend

--- slim/src/App/Controllers/UserController.php
+++ slim/src/App/Controllers/UserController.php
@@ -125,7 +125,8 @@ class UserController {
         'email' => $user['email'],
         'name' => $user['name'],
         'balance' => (float) $user['balance'],
-        'portfolio_value' => (float) $portfolioValue
+        'portfolio_value' => (float) $portfolioValue,
+        'is_admin' => (int) $user['is_admin']
         ]));
 
         return $response->withHeader('Content-Type', 'application/json')
