91c955afb09e20a5bf2701be6274a8d918b9b3b0
Laura Cuenca <laura.cuenca1@gmail.com>
2026-06-24 12:15:25 -0300

    Agrego cambios de rama magui

--- frontend/src/services/apiServices.js
+++ frontend/src/services/apiServices.js
@@ -24,14 +24,13 @@ export const buyAsset = (assetId, quantity) => api.post('/trade/buy', { asset_id
 
 export const sellAsset = (assetId, quantity) => api.post('/trade/sell', { asset_id: assetId, quantity });
 
+export const getPortfolio = () => api.get('/portfolio');
+
+export const deletePortfolioAsset = (assetId) => api.delete(`/portfolio/${assetId}`);
+
 export const getTransactions = (type = "", assetId = "") => {
   const params = {};
 
-<<<<<<< HEAD
-export const getPortfolio = () =>  api.get('/portfolio');
-
-export const deletePortfolioAsset = (assetId) =>  api.delete(`/portfolio/${assetId}`);
-=======
   if (type !== "") {
     params.type = type;
   }
@@ -42,4 +41,3 @@ export const deletePortfolioAsset = (assetId) =>  api.delete(`/portfolio/${asset
 
   return api.get("/transactions", { params });
 };
->>>>>>> magui
